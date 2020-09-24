7847c7981b752a9898a8063e3646e577c09f1fb1
Derly Viviana Murcia Serrano <49003154+derlyvivianamurcia@users.noreply.github.com>
2020-09-24 11:29:13 -0500

    Cambios, Footer

--- src/routers/Aplicacion.jsx
+++ src/routers/Aplicacion.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Insigniapages from "../pages/InsigniaPages";
 import InsigniaNueva from "../pages/InsigniaNueva";
 import Layout from "../components/Layout";
-import Footer from "../components/Footer";
+
 
 function Aplicacion() {
   return (
@@ -14,7 +14,7 @@ function Aplicacion() {
         <Route exact path='/' component={Insigniapages}/>
         <Route exact path='/insignianueva' component={InsigniaNueva}/>
       </Switch>
-      <Footer/>
+    
       </Layout>
     </BrowserRouter>
   );
